8db6543519a18b2d0fa39dde4cbd13862775a473
LexanderO <alexanderowens1993@gmail.com>
2019-03-12 22:23:22 +0000

    Update PlayerMotor.cs
    Added constant movement to Player Object

--- Endless Run/Assets/Scripts/PlayerMotor.cs
+++ Endless Run/Assets/Scripts/PlayerMotor.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
+    private CharacterController controller;
+
+    private float speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        controller.Move((Vector3.forward * speed) * Time.deltaTime);
     }
 }
