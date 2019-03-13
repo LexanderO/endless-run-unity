3b1854d10d6e2074781ce6335ff933b29b57ff0c
LexanderO <alexanderowens1993@gmail.com>
2019-03-13 22:06:29 +0000

    Update CameraMotor.cs
    Camare Follows the player but stays on same position on y axis

--- Endless Run/Assets/Scripts/CameraMotor.cs
+++ Endless Run/Assets/Scripts/CameraMotor.cs
@@ -5,6 +5,7 @@ public class CameraMotor : MonoBehaviour
 {
     private Transform lookAt;
     private Vector3 startOffset;
+    private Vector3 moveVector;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,14 @@ public class CameraMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = lookAt.position + startOffset;
+        moveVector = lookAt.position + startOffset;
+
+        //X
+        moveVector.x = 0;
+
+        //Y
+        moveVector.y = Mathf.Clamp(moveVector.y, 1, 5);
+
+        transform.position = moveVector;
     }
 }
