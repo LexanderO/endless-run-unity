1ce73026046430225e508aa1a0854fc580554778
LexanderO <alexanderowens1993@gmail.com>
2019-03-13 21:42:27 +0000

    Added Camera that follows Player

---
+++ Endless Run/Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    private Transform lookAt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = lookAt.position;
+    }
+}
