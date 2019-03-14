0cea2965ee270add5bf1f70595631b8f0237fc82
LexanderO <alexanderowens1993@gmail.com>
2019-03-14 22:12:40 +0000

    Added TileManager

---
+++ Endless Run/Assets/Scripts/TileManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    private Transform playerTransform;
+    private float spawnZ = 0.0f;
+    private float tileLength = 10.0f;
+    private int amnTilesOnScreen = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
